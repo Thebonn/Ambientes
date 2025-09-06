412b41928b44ff81ca1afd952a9e8f4d55067ef4
Bonn <thebonn07@outlook.com>
2025-09-06 20:07:44 -0300

    Atualização de pequenos detalhes

--- src/sistema/Ambiente.java
+++ src/sistema/Ambiente.java
@@ -8,28 +8,5 @@ public class Ambiente {
 
     public static void main(String[] args) {
         new gui.Splash().setVisible(true);
-
-//        sistema.GerenciadorDeSom gerenciador = new GerenciadorDeSom();
-//
-        try {
-//            gerenciador.rodar("Problem - Four tet");
-//            Thread.sleep(2000);
-//            gerenciador.pararTudo();
-//            Thread.sleep(2000);
-//            gerenciador.rodar("Teste");
-//            Thread.sleep(5000);
-//            gerenciador.pararTudo();
-//            Thread.sleep(5000);
-//            gerenciador.rodar("Problem - Four tet");
-//            Thread.sleep(5000);
-//            gerenciador.pararTudo();
-//            Thread.sleep(5000);
-//            gerenciador.rodar("Problem - Four tet");
-//             Thread.sleep(500000);
-//            
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
     }
 }
