5bd6085d67a818f7d787b4b76224a1052e176caa
Bonn <thebonn07@outlook.com>
2025-09-06 21:16:46 -0300

    Adicionado modo sem gui

--- src/sistema/Ambiente.java
+++ src/sistema/Ambiente.java
@@ -6,7 +6,23 @@ package sistema;
  */
 public class Ambiente {
 
+    
+    
     public static void main(String[] args) {
-        new gui.Splash().setVisible(true);
+        boolean semgui = false;
+        
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].contains("nogui") || args[i].contains("semgui")) {
+                semgui = true;
+                break;
+            }
+        }
+
+        if (semgui) {
+           new sistema.SemGui().principal();
+        } else {
+            new gui.Splash().setVisible(true);
+        }
+        
     }
 }
